63e8b0337c1e5332921901cc57fd66791ef08014
Robinson <robinson30122000@gmail.com>
2026-07-14 19:54:48 +0530

    v1

--- src/hooks/useMagnetic.js
+++ src/hooks/useMagnetic.js
@@ -16,11 +16,16 @@ export const useMagnetic = (options = { strength: 50 }) => {
     const xTo = gsap.quickTo(element, "x", { duration: 1, ease: "elastic.out(1, 0.3)" });
     const yTo = gsap.quickTo(element, "y", { duration: 1, ease: "elastic.out(1, 0.3)" });
 
+    let bounds = { width: 0, height: 0, left: 0, top: 0 };
+
+    const handleMouseEnter = () => {
+      bounds = element.getBoundingClientRect();
+    };
+
     const handleMouseMove = (e) => {
       const { clientX, clientY } = e;
-      const { height, width, left, top } = element.getBoundingClientRect();
-      const x = clientX - (left + width / 2);
-      const y = clientY - (top + height / 2);
+      const x = clientX - (bounds.left + bounds.width / 2);
+      const y = clientY - (bounds.top + bounds.height / 2);
       
       xTo(x * (options.strength / 100));
       yTo(y * (options.strength / 100));
@@ -31,10 +36,12 @@ export const useMagnetic = (options = { strength: 50 }) => {
       yTo(0);
     };
 
+    element.addEventListener('mouseenter', handleMouseEnter);
     element.addEventListener('mousemove', handleMouseMove);
     element.addEventListener('mouseleave', handleMouseLeave);
 
     return () => {
+      element.removeEventListener('mouseenter', handleMouseEnter);
       element.removeEventListener('mousemove', handleMouseMove);
       element.removeEventListener('mouseleave', handleMouseLeave);
     };
